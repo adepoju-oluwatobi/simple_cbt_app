de70373155ee028876e00d5ce743ec99d11325bf
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-10-23 02:05:05 +0100

    added a virtual environment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import json
 import os
+import random
 
 from flask import Flask, render_template, flash, request, redirect, url_for, session
 
@@ -7,12 +8,13 @@ from flask import Flask, render_template, flash, request, redirect, url_for, ses
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__)
-app.secret_key = 'username'  # Replace with your own secret key
+app.secret_key = 'username'
 print(app.secret_key)
 
 # Specify the full paths to the JSON files
 students_json_path = os.path.join(current_directory, 'student_data/student.json')
 teachers_json_path = os.path.join(current_directory, 'teacher_data/teacher.json')
+admin_json_path = os.path.join(current_directory, 'admin_data/admin.json')
 questions_json_path = os.path.join(current_directory, 'questions/questions.json')
 
 # Declare a dictionary to store teacher-student assignments
@@ -25,21 +27,30 @@ with open(students_json_path, 'r') as students_data:
 with open(teachers_json_path, 'r') as teachers_data:
     teachers = json.load(teachers_data)
 
+with open(admin_json_path, 'r') as admin_data:
+    admin = json.load(admin_data)
+
 with open(questions_json_path, 'r') as questions_file:
     questions = json.load(questions_file)
 
 
+# handles user authentication
 def is_authenticated():
     return 'username' in session  # Return True if 'username' is in the session
 
 
+# route for the landing page
 @app.route('/')
 def homepage():
     return render_template('landingPage.html', user_authenticated=is_authenticated())
 
 
+# student login route
 @app.route('/student/login', methods=['GET', 'POST'])
 def student_login():
+    user_authenticated = is_authenticated()  # Check if the user is authenticated
+    user_role = "student"  # Set the user role
+
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -59,9 +70,11 @@ def student_login():
 
             return redirect(url_for('student_dashboard'))
 
-        return render_template('student/login.html', error="Invalid credentials", user_authenticated=is_authenticated())
+        return render_template('student/login.html', error="Invalid credentials", user_authenticated=user_authenticated, user_role=user_role)
 
-    return render_template('student/login.html')
+    return render_template('student/login.html', user_authenticated=user_authenticated, user_role=user_role)
+
+
 
 
 def get_teacher_for_class(student_class):
@@ -77,6 +90,9 @@ def teacher_login():
         username = request.form['username']
         password = request.form['password']
 
+        user_authenticated = True
+        user_role = "teacher"
+
         print(f"Received username: {username}, password: {password}")
 
         # Simplified authentication (replace with your authentication logic)
@@ -84,7 +100,7 @@ def teacher_login():
             session['username'] = username
             return redirect(url_for('teacher_dashboard'))
 
-        return render_template('teacher/login.html', error="Invalid credentials", user_authenticated=is_authenticated())
+        return render_template('teacher/login.html', error="Invalid credentials", user_authenticated=is_authenticated(), user_role=user_role)
 
     return render_template('teacher/login.html')
 
@@ -150,6 +166,9 @@ def manage_student():
     if 'username' in session:
         teacher_username = session['username']
         assigned_class = teachers[teacher_username]['class']
+
+        # Load the updated student data from the JSON file
+        students = load_student_data()
 
         # Filter students based on class
         filtered_students = {username: student_data for username, student_data in students.items() if
@@ -480,14 +499,6 @@ def load_available_exams():
     return exams_data
 
 
-# @app.route('/edit_questions/<class_name>/<subject>')
-# def edit_exam(class_name, subject):
-#     # Your edit exam logic goes here
-#     # ...
-#     return render_template('teacher/edit_questions.html', class_name=class_name, subject=subject,
-#                            questions_data=questions)
-
-
 @app.route('/delete_exam/<class_name>/<subject>')
 def delete_exam(class_name, subject):
     # Load your questions JSON data
@@ -547,6 +558,187 @@ def update_questions_for_class_subject(class_name, subject, edited_questions_dat
         # Save the updated questions data back to the 'questions.json' file
         with open(questions_json_path, 'w') as questions_file:
             json.dump(questions, questions_file, indent=4)
+
+
+@app.route('/admin/login', methods=['GET', 'POST'])
+def admin_login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        print(f"Received username: {username}, password: {password}")
+
+        # Simplified authentication (replace with your authentication logic)
+        if username in admin and admin[username]['password'] == password:
+            session['username'] = username
+
+            # Check for a teacher assigned to the student's class
+            # student_class = students[username]['class']
+            # assigned_teacher = get_teacher_for_class(student_class)
+
+            # if assigned_teacher:
+            #     session['assigned_teacher'] = assigned_teacher
+
+            return redirect(url_for('admin_dashboard'))
+
+        return render_template('admin/login.html', error="Invalid credentials", user_authenticated=is_authenticated())
+
+    return render_template('admin/login.html')
+
+
+@app.route('/admin/dashboard')
+def admin_dashboard():
+    if 'username' in session:
+        username = session['username']
+        admin_data = teachers.get(username, {})  # Get the user's data
+
+        return render_template('admin/dashboard.html', username=username)
+    else:
+        return redirect(url_for('admin/login'))
+
+
+def load_students_data():
+    with open('student_data/student.json', 'r') as student_file:
+        student_data = json.load(student_file)
+    return student_data
+
+
+@app.route('/admin/manage_student')
+def manage_all_students():
+    if 'username' in session:
+        # Load your student data here (replace with your actual data loading logic)
+        students_data = load_students_data()
+
+        return render_template('admin/manage_students.html', students=students_data)
+    else:
+        return redirect(url_for('admin/login'))
+
+
+@app.route('/edit_student/<username>')
+def edit_student(username):
+    # Add logic to retrieve the student's data using the username
+    # and render the 'edit_student.html' template with the data.
+    student = get_student_data(username)
+    return render_template('admin/edit_student.html', username=username, student=student)
+
+
+@app.route('/save_edited_student/<username>', methods=['POST'])
+def save_edited_student(username):
+    if request.method == 'POST':
+        name = request.form['name']
+        student_class = request.form['class']
+        parent_name = request.form['parent_name']
+
+        updated_data = {
+            'name': name,
+            'class': student_class,
+            'parent_name': parent_name
+        }
+
+        update_student_data(username, updated_data)
+
+        return redirect(url_for('manage_all_students'))
+    else:
+        return "Invalid request"
+
+
+def get_student_data(username):
+    # Load student data from the JSON file
+    with open(students_json_path, 'r') as students_data_file:
+        students_data = json.load(students_data_file)
+
+    # Retrieve the student data by username
+    return students_data.get(username)
+
+
+# Function to update student data
+def update_student_data(username, updated_data):
+    # Load student data from the JSON file
+    with open(students_json_path, 'r') as students_data_file:
+        students_data = json.load(students_data_file)
+
+    # Update the student data for the specified username
+    if username in students_data:
+        students_data[username].update(updated_data)
+
+        # Save the updated data back to the JSON file
+        with open(students_json_path, 'w') as students_data_file:
+            json.dump(students_data, students_data_file, indent=4)
+
+
+@app.route('/admin/delete_student/<username>', methods=['GET', 'POST'])
+def admin_delete_student(username):
+    if request.method == 'POST':
+        # Check if the user has confirmed the deletion
+        if 'confirm' in request.form:
+            # Load the existing student data
+            students = load_students_data()
+
+            if username in students:
+                # Remove the student with the provided username
+                del students[username]
+
+                # Save the updated student data back to the JSON file
+                save_students_data(students)
+
+                return redirect(url_for('manage_all_students'))
+        else:
+            # The user did not confirm the deletion
+            return redirect(url_for('manage_all_students'))
+
+    # If it's a GET request, display a confirmation page
+    return render_template('admin/confirm_delete_student.html', username=username)
+
+
+# Add a new route to display the form for adding a new student
+@app.route('/admin/add_student', methods=['GET', 'POST'])
+def add_student():
+    if request.method == 'POST':
+        # Handle form submission to add the new student
+        parent_name = request.form['parent_name']
+        name = request.form['name']
+        student_class = request.form['class']
+        password = request.form['password']
+
+        # Create a new student record (you can add more data fields as needed)
+        new_student = {
+            'parent_name': parent_name,
+            'name': name,
+            'class': student_class,
+            'password': password,
+            'score': 0  # You can set an initial score if needed
+        }
+
+        # Load the existing student data from the JSON file
+        students_data = load_students_data()
+
+        # Ensure the username is unique (you can add your logic for generating a unique username)
+        username = generate_unique_username(name)
+
+        # Add the new student to the dictionary with the unique username
+        students_data[username] = new_student
+
+        # Save the updated student data back to the JSON file
+        save_students_data(students_data)
+
+        return redirect(url_for('manage_all_students'))
+    else:
+        return render_template('admin/add_student.html')
+
+
+# Function to save student data to the JSON file
+def save_students_data(students_data):
+    with open(students_json_path, 'w') as students_file:
+        json.dump(students_data, students_file, indent=4)
+
+
+# Define a function to generate a unique username (you can customize this logic)
+def generate_unique_username(name):
+    # generate a random username
+    first_name = name.split()[0].lower()
+    random_number = random.randint(1000, 9999)
+    username = f"{first_name}{random_number}"
+    return username
 
 
 if __name__ == '__main__':
